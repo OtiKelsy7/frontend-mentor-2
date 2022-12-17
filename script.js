ff829f6aba64062d4e3e0af1e7a8d394089c07a6
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2022-12-17 04:26:01 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let PopUp = document.getElementById("completed-div");
+
+function openBtn() {
+    PopUp.classList.add("visible-pop")
+}
+
+function closeBtn() {
+    PopUp.classList.remove("visible-pop")
+}
