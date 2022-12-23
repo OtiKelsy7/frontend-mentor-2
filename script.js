77265ca8bf4ddf194a61ef5db19cc412832aba38
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2022-12-23 14:25:50 -0800

    bugs encountered

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let PopUp = document.getElementById("completed-div");
 
 function openBtn() {
     PopUp.classList.add("visible-pop")
-}
+};
 
 function closeBtn() {
     PopUp.classList.remove("visible-pop")
+};
+
+function numFunc(_e) {
+    console.log(numFunc())
 }
