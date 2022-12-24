abe30784a3558a301c3546deaaef1978a6a23545
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2022-12-24 13:34:53 -0800

    improvements made

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-let PopUp = document.getElementById("completed-div");
+//initial references
+const PopUp = document.getElementById("completed-div");
 
+const submitBtn = document.getElementById("submitButton");
+
+const numDiv = document.getElementById("number-selection");
+
+// const numDiv1 = document.getElementById("number-selection1");
+
+// const numDiv2 = document.getElementById("number-selection2");
+
+// const numDiv3 = document.getElementById("number-selection3");
+
+// const numDiv4 = document.getElementById("number-selection4");
+
+const intDiv = document.getElementById("interactive-div");
+
+
+//functionalities start
 function openBtn() {
     PopUp.classList.add("visible-div")
-};
+}
 
-function closeBtn() {
-    PopUp.classList.remove("visible-div")
-};
+submitBtn.addEventListener("click", () => {
+    console.log("button clicked")
+})
+
+numDiv.addEventListener("click", () => {
+    console.log("button clicked")
+})
+
+// numDiv.addEventListener("click", () => {
+//     console.log("button 1 clicked")
+// })
+// numDiv.addEventListener("click", () => {
+//     console.log("button 2 clicked")
+// })
+// numDiv.addEventListener("click", () => {
+//     console.log("button 3 clicked")
+// })
+// numDiv.addEventListener("click", () => {
+//     console.log("button 4 clicked")
+// })
+
+
+// function closeBtn() {
+//     PopUp.classList.remove("visible-div")
+// };
 
 function numFunc(num) {
     return num;
@@ -14,8 +53,4 @@ function numFunc(num) {
 
 console.log(numFunc())
 
-// let btnEl = document.getElementsByClassName("select-btn");
-
-// btnEl.addEventListener("click", ()=> {
-    
-// })
+//functionalities end
