a4de1b2f6fb518facd88641b0c6aeb4cb0c5f4c1
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2022-12-24 00:54:26 -0800

    new commit

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let PopUp = document.getElementById("completed-div");
 
 function openBtn() {
-    PopUp.classList.add("visible-pop")
+    PopUp.classList.add("visible-div")
 };
 
 function closeBtn() {
-    PopUp.classList.remove("visible-pop")
+    PopUp.classList.remove("visible-div")
 };
 
-function numFunc(_e) {
-    console.log(numFunc())
+function numFunc(num) {
+    return num;
 }
+
+console.log(numFunc())
+
+// let btnEl = document.getElementsByClassName("select-btn");
+
+// btnEl.addEventListener("click", ()=> {
+    
+// })
